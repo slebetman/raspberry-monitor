56cf475e08bdb2e05ed83b63af50882a3a5648aa
slebetman <slebetman@gmail.com>
2024-02-12 09:43:28 +0800

    Add checkbox to see tasks for all users

--- components/task-manager.js
+++ components/task-manager.js
@@ -2,7 +2,7 @@ const component = require('express-htmx-components');
 const { html, css } = require('express-htmx-components/tags');
 const ps = require('./lib/ps');
 
-const get = component.get('/task-manager', async ({}) => {
+const get = component.get('/task-manager', async ({ all }) => {
 	return html`
 	<style>${style}</style>
 	<h2>
@@ -11,12 +11,30 @@ const get = component.get('/task-manager', async ({}) => {
 		</span>
 		Task Manager
 	</h2>
+	<form>
+		<label for="select-all">Show all processes:</label>
+		$${ all ? html`
+			<input type="checkbox"
+				id="select-all"
+				hx-get="/task-manager"
+				hx-target="#content"
+				checked
+			>`
+			:
+			html`
+			<input type="checkbox"
+				id="select-all"
+				hx-get="/task-manager?all=true"
+				hx-target="#content"
+			>`
+		}
+	</form>
 	<div class="section">
 		<div id="ps-container"
-			hx-get="/ps"
+			hx-get="/ps${ all ? '?all=true' : '' }"
 			hx-trigger="every 3637ms"
 		>
-			$${await ps.get.html({ all: false })}
+			$${await ps.get.html({ all })}
 		</div>
 	</div>
 	`;
@@ -25,7 +43,7 @@ const get = component.get('/task-manager', async ({}) => {
 const style = css`
 	@media (max-device-width: 920px) {
 		#ps-container {
-			height: calc(100vh - 230px);
+			height: calc(100vh - 265px);
 			overflow: scroll;
 		}
 	}
